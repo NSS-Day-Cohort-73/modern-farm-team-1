f2a867dbb5011dfb1746ef6c9219fe31a093c1a4
Jonathan Castro <jonathancastro55@gmail.com>
2024-09-20 14:12:03 -0500

    Added in catalogue functiona and moduele

--- src/scripts/catalogue.js
+++ src/scripts/catalogue.js
@@ -1,2 +1,7 @@
-
-export const
+export const Catalog = (harvestedPlants) => {
+    const container = document.querySelector('.container');
+    harvestedPlants.forEach(plant => {
+        const plantHTML = `<section class="plant">${plant.name}</section>`;
+        container.innerHTML += plantHTML;
+    });
+};
