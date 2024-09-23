6e304650d89f87bbe71ff91ce9d58004b0197bc9
Jonathan Castro <jonathancastro55@gmail.com>
2024-09-23 10:17:57 -0500

    Conlfict

--- src/scripts/harvester.js
+++ src/scripts/harvester.js
@@ -3,18 +3,16 @@ export const harvestPlants = (arr) => {
     for (const item of arr) {
         if (Array.isArray(item)) {
             for (const object of item) {
-                const output = object.output / 2
-                for (let i=0; i < output; i++) {
-                    harvestArray.push(object)
+                const output = object.output / 2 
+                for (let i = 0; i < output; i++) {
+                    harvestArray.push(object) 
                 }
-
             }
         } else {
-            for (let x=0; x < item.output; x++) {
-                harvestArray.push(item)
+            for (let x = 0; x < item.output; x++) {
+                harvestArray.push(item)  
             }
         }
     }
     return harvestArray
 }
-
